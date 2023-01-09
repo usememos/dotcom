90d603e9f54569f376841e3d6732dd48aa739c32
Steven <stevenlgtm@gmail.com>
2023-01-09 23:19:51 +0800

    chore: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
