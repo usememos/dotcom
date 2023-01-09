0a31a905de3737ad85fd7465721ad0d81ebcf9c5
Steven <stevenlgtm@gmail.com>
2023-01-09 22:57:10 +0800

    feat: add feature pages

--- components/FeatureMatrix.tsx
+++ components/FeatureMatrix.tsx
@@ -1,6 +1,7 @@
 import Icon from "./Icon";
 
 interface FeatureItem {
+  slug: string;
   icon: Icon.Icon;
   title: string;
   description: string;
@@ -8,31 +9,37 @@ interface FeatureItem {
 
 export const FEATURE_LIST: FeatureItem[] = [
   {
+    slug: "privacy-first",
     icon: Icon.Shield,
     title: "Privacy First",
     description: "Keep your own data by yourself. All data generated at runtime is saved in the SQLite database file.",
   },
   {
+    slug: "plain-text",
     icon: Icon.Edit3,
     title: "Plain text with Markdown",
     description: "All content will be saved as plain text, not HTML. And lots of useful markdown syntax are supported.",
   },
   {
+    slug: "lightweight",
     icon: Icon.Compass,
     title: "Lightweight but Powerful",
     description: "Using Go + React.js + SQLite architecture, the overall package is very lightweight.",
   },
   {
+    slug: "customizable",
     icon: Icon.Sliders,
     title: "Customizable",
     description: "You can customize the server name, icon, description, custom system style and execution script, etc.",
   },
   {
+    slug: "open-source",
     icon: Icon.GitHub,
     title: "Open Source Completely",
     description: "memos believes that open source is the future, and all code is already open source in GitHub.",
   },
   {
+    slug: "free-forever",
     icon: Icon.Smile,
     title: "Free Forever",
     description: "All features are free forever and will never be charged in any form or content.",
@@ -49,11 +56,15 @@ const FeatureMatrix = () => {
       </div>
       <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 px-4 sm:px-16">
         {FEATURE_LIST.map((featureItem) => (
-          <div key={featureItem.title} className="w-full flex flex-col justify-start items-start mb-2 sm:mb-8">
+          <a
+            key={featureItem.title}
+            href={`/feature/${featureItem.slug}`}
+            className="w-full flex flex-col justify-start items-start mb-2 sm:mb-8"
+          >
             <featureItem.icon className="h-10 mt-4 mb-2 sm:h-14 w-auto text-gray-500" />
             <span className="text-base sm:text-xl sm:leading-10 my-1">{featureItem.title}</span>
             <p className="text-sm sm:text-base text-gray-500">{featureItem.description}</p>
-          </div>
+          </a>
         ))}
       </div>
     </>
---
+++ pages/feature/[featureName].tsx
@@ -0,0 +1,78 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import FeatureMatrix, { FEATURE_LIST } from "../../components/FeatureMatrix";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+const Feature: NextPage = () => {
+  const router = useRouter();
+  const { featureName } = router.query;
+  const feature = FEATURE_LIST.find((feature) => feature.slug === featureName);
+
+  return (
+    <div className="px-4 sm:px-0 bg-zinc-100">
+      <Head>
+        <title>
+          memos - {feature?.title} - {feature?.description}
+        </title>
+        <link rel="icon" href="/logo.png" />
+        <meta name="description" content="An open-source, self-hosted memo hub with knowledge management and socialization." />
+        <meta property="og:url" content="https://usememos.com/" />
+        <meta name="og:title" content="memos" />
+        <meta name="og:description" content="An open-source, self-hosted memo hub with knowledge management and socialization." />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+      </Head>
+
+      <Header />
+
+      <main className="min-h-screen w-full max-w-4xl mx-auto flex flex-col justify-start items-start pt-4 pb-16">
+        <div className="w-full flex flex-col justify-start items-center">
+          <img className="h-24 sm:h-32 w-auto" src="/logo.png" alt="logo" />
+          <h1 className="text-3xl -mt-4 text-zinc-700">memos</h1>
+        </div>
+        <div className="w-full flex flex-col justify-center items-center sm:px-16">
+          <h2 className="w-full text-center text-4xl sm:text-6xl font-bold mt-4 mb-4">{feature?.title}</h2>
+          <h3 className="w-full text-base sm:text-lg text-gray-500 text-center mb-2">{feature?.description}</h3>
+        </div>
+        <div className="w-full flex flex-row justify-center items-center space-x-2 py-4">
+          <a target="_blank" href="https://github.com/usememos/memos" rel="noreferrer">
+            <img alt="GitHub stars" src="https://img.shields.io/github/stars/usememos/memos?logo=github" />
+          </a>
+          <a target="_blank" href="https://hub.docker.com/r/neosmemo/memos" rel="noreferrer">
+            <img alt="Docker" src="https://img.shields.io/docker/pulls/neosmemo/memos.svg?logo=docker" />
+          </a>
+          <a target="_blank" href="https://discord.gg/tfPJa4UmAv" rel="noreferrer">
+            <img alt="Discord" src="https://img.shields.io/badge/discord-chat-5865f2?logo=discord" />
+          </a>
+        </div>
+        <div className="w-full flex flex-row justify-center items-center space-x-2">
+          <a
+            href="https://demo.usememos.com"
+            target="_blank"
+            className="text-blue-600 leading-6 text-base hover:underline"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+          <span className="block mx-2">•</span>
+          <a
+            href="https://discord.gg/tfPJa4UmAv"
+            target="_blank"
+            className="text-blue-600 leading-6 text-base hover:underline"
+            rel="noreferrer"
+          >
+            Discuss in Discord 🙋
+          </a>
+        </div>
+        <img className="w-full h-auto mt-2 mb-2" src="/demo.png" alt="demo-screenshot" />
+        <FeatureMatrix />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Feature;
