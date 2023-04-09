9768fa52974743a2eb679233110846a223009599
Steven <stevenlgtm@gmail.com>
2023-04-09 19:42:43 +0800

    docs: update author view

---
+++ consts/author.ts
@@ -0,0 +1,19 @@
+interface Author {
+  name: string;
+  email?: string;
+  url?: string;
+  github?: string;
+  twitter?: string;
+  funding?: string;
+}
+
+const authorList: Author[] = [
+  {
+    name: "Steven",
+    github: "boojack",
+    twitter: "stevenlgtm",
+    funding: "https://ko-fi.com/stevenlgtm",
+  },
+];
+
+export default authorList;
