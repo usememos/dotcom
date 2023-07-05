ead5f581900a0cf461e3546f4407c332991813df
Steven <stevenlgtm@gmail.com>
2023-07-05 09:28:12 +0800

    chore: update shortify banner

--- components/ShortifyBanner.tsx
+++ components/ShortifyBanner.tsx
@@ -1,12 +1,12 @@
 const ShortifyBanner = () => {
   return (
     <a
-      className="mt-4 mb-2 px-2 py-1 rounded-lg bg-blue-50 shadow hover:underline hover:text-blue-600"
+      className="mt-4 mb-2 px-2 py-1 rounded-lg bg-blue-50 shadow text-blue-600 hover:underline"
       href="https://github.com/boojack/shortify"
       target="_blank"
     >
       <img className="w-6 h-auto mr-1 inline-block -mt-0.5" src="https://github.com/boojack/shortify/raw/main/resources/logo.png" alt="" />
-      <span className="font-bold">Shortify</span>: Your bookmarking and short link service.
+      <span className="font-bold">Shortify</span>: Your bookmarking and url shortener.
     </a>
   );
 };
