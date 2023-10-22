19e6ce239ca5e0f76914f7a45d004fdd60302d09
Steven <stevenlgtm@gmail.com>
2023-10-22 23:41:58 +0800

    chore: use src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("@tailwindcss/typography")],
 };
