cc27929a7c7e15c837694c256a6a0693d977126b
Steven <stevenlgtm@gmail.com>
2023-10-25 00:58:34 +0800

    chore: tweak brand display

--- src/app/brand/page.tsx
+++ src/app/brand/page.tsx
@@ -6,19 +6,21 @@ const Page = () => {
       <div className="w-full max-w-3xl flex flex-col justify-center items-center sm:px-16">
         <h2 className="w-full text-center text-4xl sm:text-6xl font-medium sm:font-bold mt-4 mb-6">Brand</h2>
 
-        <div className="mt-12 w-full grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-12">
+        <div className="mt-12 w-full grid grid-cols-2 gap-6 sm:gap-12">
           <div className="col-span-1 border p-4 flex justify-center items-center">
-            <img src="/logo.png" alt="" />
+            <img src="/logo.png" alt="memos" />
           </div>
           <div className="col-span-1 border p-4 flex justify-center items-center">
-            <img src="/logo-rounded.png" alt="" />
-          </div>
-          <div className="col-span-1 border p-4 flex justify-center items-center">
-            <img src="/full-logo.png" alt="" />
+            <img src="/logo-rounded.png" alt="memos" />
           </div>
         </div>
-        <div className="mt-6 sm:mt-12 w-full border p-4 flex justify-center items-center">
-          <img src="/full-logo-landscape.png" alt="" />
+        <div className="mt-6 sm:mt-12 w-full grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-12">
+          <div className="col-span-1 sm:col-span-2 border p-4 flex justify-center items-center">
+            <img src="/full-logo-landscape.png" alt="memos" />
+          </div>
+          <div className="col-span-1 border p-4 flex justify-center items-center">
+            <img src="/full-logo.png" alt="memos" />
+          </div>
         </div>
       </div>
     </>
