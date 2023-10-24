e2803fd673bb00c31f4eced2ce6f8c4c5e12745d
Steven <stevenlgtm@gmail.com>
2023-10-25 00:58:56 +0800

    chore: tweak brand style

--- src/app/brand/page.tsx
+++ src/app/brand/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
       <div className="w-full max-w-3xl flex flex-col justify-center items-center sm:px-16">
         <h2 className="w-full text-center text-4xl sm:text-6xl font-medium sm:font-bold mt-4 mb-6">Brand</h2>
 
-        <div className="mt-12 w-full grid grid-cols-2 gap-6 sm:gap-12">
+        <div className="mt-6 sm:mt-12 w-full grid grid-cols-2 gap-6 sm:gap-12">
           <div className="col-span-1 border p-4 flex justify-center items-center">
             <img src="/logo.png" alt="memos" />
           </div>
