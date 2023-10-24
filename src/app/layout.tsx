a254cbf21cb242692e1e6d12f232459c719981bd
Steven <stevenlgtm@gmail.com>
2023-10-25 07:54:36 +0800

    chore: add posthog provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,33 @@
 import { Analytics } from "@vercel/analytics/react";
-import React from "react";
+import React, { Suspense } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { PHProvider, PostHogPageview } from "@/components/PosthogProvider";
 import "@/styles/global.css";
 import { getMetadata } from "@/utils/metadata";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
         <link rel="icon" href="/favicon/favicon.png" type="image/png" />
       </head>
       <body>
-        <div className="relative w-full flex flex-col">
-          <Header />
+        <Suspense>
+          <PostHogPageview />
+        </Suspense>
+        <PHProvider>
+          <div className="relative w-full flex flex-col">
+            <Header />
 
-          <main className="relative shrink-0 grow basis-auto mx-auto pt-6 pb-16 px-6 w-full max-w-6xl flex flex-col justify-start items-center">
-            {children}
-          </main>
-          <Footer />
-        </div>
-        <Analytics />
-        <script async src="https://analytics.eu.umami.is/script.js" data-website-id="b6e36293-961b-43eb-af79-d7da00f27707"></script>
+            <main className="relative shrink-0 grow basis-auto mx-auto pt-6 pb-16 px-6 w-full max-w-6xl flex flex-col justify-start items-center">
+              {children}
+            </main>
+            <Footer />
+          </div>
+          <Analytics />
+        </PHProvider>
       </body>
     </html>
   );
---
+++ src/components/PosthogProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import React, { useEffect } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init("phc_Ohx7qYjHKRAnrgp1Y6ohGEP5EoyLyPi9kBiSAzzPzEX", {
+    api_host: "https://app.posthog.com",
+    capture_pageview: false,
+  });
+}
+
+export function PostHogPageview() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      let url = window.origin + pathname;
+      if (searchParams && searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return <></>;
+}
+
+export function PHProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
