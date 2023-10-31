a9df926e87f0f8b91cb0430436c4e8dd4f0cd7bc
Steven <stevenlgtm@gmail.com>
2023-10-31 22:13:25 +0800

    chore: update subscription

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -16,7 +16,6 @@ const Subscription = ({ className }: Props) => {
 
   const handleEmailChanged = (value: string) => {
     setEmail(value);
-    setSubscribed(false);
   };
 
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,6 +24,9 @@ const Subscription = ({ className }: Props) => {
   };
 
   const handleSubscribeButtonClick = () => {
+    if (subscribed) {
+      return;
+    }
     if (!validateEmail(email)) {
       toast.error("Invalid email");
       return;
@@ -40,25 +42,28 @@ const Subscription = ({ className }: Props) => {
   return (
     <div className={classNames("w-full shadow border rounded-2xl p-4 sm:p-8", className)}>
       <div className="mb-2 sm:mb-4 flex flex-row justify-start items-center">
-        <span className="text-3xl sm:text-6xl mr-2">📧</span>
+        <span className="text-3xl sm:text-6xl mr-2">💌</span>
         <span className="text-lg sm:text-2xl md:text-3xl leading-tight">Get the latest news of Memos.</span>
       </div>
       <form onSubmit={handleFormSubmit} className="flex flex-row justify-start items-center gap-4">
-        <Input
-          className="grow"
-          type="email"
-          placeholder="Type your email..."
-          value={email}
-          onChange={(event) => handleEmailChanged(event.target.value)}
-        />
+        {!subscribed && (
+          <Input
+            className="grow"
+            type="email"
+            placeholder="Type your email..."
+            value={email}
+            onChange={(event) => handleEmailChanged(event.target.value)}
+          />
+        )}
         <Button
+          className={classNames(subscribed && "w-full")}
           type="submit"
           variant="outlined"
           color="primary"
-          disabled={!validateEmail(email) || subscribed}
+          disabled={!validateEmail(email)}
           onClick={handleSubscribeButtonClick}
         >
-          {subscribed ? "Subscribed!" : "Subscribe"}
+          {subscribed ? "Subscription successful!" : "Subscribe"}
         </Button>
       </form>
     </div>
