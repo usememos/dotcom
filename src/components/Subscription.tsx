4ea51e71c24fc196acefa1de5c06e7afbaa24485
Steven <stevenlgtm@gmail.com>
2024-01-22 08:23:47 +0800

    chore: update subscription bg color

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -39,7 +39,7 @@ const Subscription = ({ className }: Props) => {
   };
 
   return (
-    <div className={classNames("w-full shadow border rounded-2xl p-4 sm:p-8", className)}>
+    <div className={classNames("w-full bg-white shadow border rounded-2xl p-4 sm:p-8", className)}>
       <div className="mb-2 sm:mb-4 flex flex-row justify-start items-center">
         <span className="text-3xl sm:text-6xl mr-2">💌</span>
         <span className="text-lg sm:text-2xl md:text-3xl leading-tight">Get the latest news of Memos</span>
