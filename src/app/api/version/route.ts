1762a3eba34eb4182f687965ca6dede22a24f966
Steven <stevenlgtm@gmail.com>
2024-05-16 08:56:13 +0800

    chore: update version api

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -1,5 +1,5 @@
 export const GET = async () => {
-  const res = await fetch("https://api.github.com/repos/usememos/memos/tags", {
+  const res = await fetch("https://api.github.com/repos/usememos/memos/releases", {
     headers: {
       Accept: "application/vnd.github.v3.star+json",
       Authorization: "",
