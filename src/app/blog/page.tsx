28e57f3ef74ff6b5367578753d35e429a7cbd73e
yourselfhosted <yourselfhosted@gmail.com>
2024-05-26 16:21:54 +0800

    chore: update blog styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,8 @@
-import { Divider } from "@mui/joy";
+import dayjs from "dayjs";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import React from "react";
 import Icon from "@/components/Icon";
 import authorList, { Author } from "@/consts/author";
 import { getContentFilePaths, getFilePathFromSlugs, readFileContenxt } from "@/lib/content";
@@ -17,33 +18,45 @@ const Page = () => {
 
   return (
     <>
-      <div className="w-full max-w-3xl flex flex-col justify-center items-center sm:px-16">
-        <h1 className="w-full text-2xl text-center sm:text-4xl font-medium sm:font-bold mt-4">Blog</h1>
-        <h2 className="text-lg mt-4">Get the latest news about Memos</h2>
-        <div className="mt-12 w-full flex flex-col justify-start items-start gap-8">
+      <div className="w-full mx-auto py-2 sm:px-4 flex flex-col justify-start items-start">
+        <h1 className="w-full text-2xl text-start sm:text-4xl font-medium sm:font-bold mt-4">Blogs</h1>
+        <h2 className="text-lg mt-4">Get the latest articles from Memos</h2>
+        <div className="mt-6 w-full flex flex-col justify-start items-start gap-4 sm:gap-6">
           {frontmatters.map((frontmatter) => {
             const author = authorList.find((author) => author.name === frontmatter.author) as Author;
 
             return (
-              <>
-                <div key={frontmatter.slug} className="w-full flex flex-col justify-start items-start">
+              <React.Fragment key={frontmatter.slug}>
+                <div className="w-full bg-white p-4 border rounded-xl flex flex-row justify-start items-start gap-x-4">
+                  <div className="flex flex-1 flex-col sm:p-2 justify-start items-start">
+                    {frontmatter.feature_image && (
+                      <Link
+                        className="w-full sm:hidden mb-4 shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
+                        href={`/blog/${frontmatter.slug}`}
+                      >
+                        <img src={frontmatter.feature_image} alt="" />
+                      </Link>
+                    )}
+                    <Link className="text-lg !leading-tight sm:text-xl line-clamp-2 hover:text-blue-600" href={`/blog/${frontmatter.slug}`}>
+                      {frontmatter.title}
+                    </Link>
+                    {frontmatter.description && <p className="mt-2 text-gray-400 line-clamp-2">{frontmatter.description}</p>}
+                    <div className="mt-2 w-full flex flex-row justify-start items-center text-gray-500">
+                      <span>{author.name}</span>
+                      <Icon.Dot />
+                      <span>{dayjs(frontmatter.published_at).format("DD/MM/YYYY")}</span>
+                    </div>
+                  </div>
                   {frontmatter.feature_image && (
-                    <Link className="mb-4 rounded-lg overflow-clip hover:opacity-80 hover:shadow" href={`/blog/${frontmatter.slug}`}>
-                      <img src={frontmatter.feature_image} alt="" />
+                    <Link
+                      className="hidden sm:block shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
+                      href={`/blog/${frontmatter.slug}`}
+                    >
+                      <img className="w-72" src={frontmatter.feature_image} alt="" />
                     </Link>
                   )}
-                  <Link className="text-lg !leading-tight sm:text-xl line-clamp-2 hover:text-blue-600" href={`/blog/${frontmatter.slug}`}>
-                    {frontmatter.title}
-                  </Link>
-                  {frontmatter.description && <p className="mt-2 text-gray-400 line-clamp-2">{frontmatter.description}</p>}
-                  <div className="mt-2 w-full flex flex-row justify-start items-center text-gray-500">
-                    <span>{author.name}</span>
-                    <Icon.Dot />
-                    <span>{frontmatter.published_at}</span>
-                  </div>
                 </div>
-                <Divider />
-              </>
+              </React.Fragment>
             );
           })}
           <Subscription className="mt-4" />
