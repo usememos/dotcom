a731ff341f4f81059f969d7658b2cef733f1ace4
Steven <stevenlgtm@gmail.com>
2024-06-06 22:25:28 +0800

    chore: update sponsors

--- src/app/supporters/page.tsx
+++ src/app/supporters/page.tsx
@@ -9,12 +9,24 @@ const SPONSORS = [
   },
 ];
 
+const GITHUB_USER_SPONSORS = [
+  {
+    title: "redmetablue",
+    logo: "https://avatars.githubusercontent.com/u/121377500?v=4",
+    url: "https://github.com/redmetablue",
+  },
+];
+
 const Page = () => {
   return (
     <>
       <div className="w-full flex flex-col justify-center items-start sm:px-12">
-        <h3 className="text-4xl sm:text-6xl leading-relaxed font-bold w-full">Thanks! 💗</h3>
-        <p className="text-xl mt-2">Thank you to our sponsors for supporting Memos. You makes it all possible!</p>
+        <h3 className="mt-12 mb-4 text-5xl sm:text-6xl leading-relaxed font-bold w-full">Thanks! 💗</h3>
+        <p className="text-xl max-w-3xl leading-normal">
+          {
+            "All donations directly support the development and operation of Memos. Recurring donations allow us to plan for the future. We deeply appreciate every donation — Thank you!"
+          }
+        </p>
         <div className="w-full flex flex-col mt-12">
           <p className="text-2xl sm:text-3xl font-medium">🦄 Sponsors</p>
           <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
@@ -41,6 +53,36 @@ const Page = () => {
               </a>
             </div>
           </div>
+        </div>
+        <div className="w-full flex flex-col mt-12">
+          <p className="text-2xl sm:text-3xl font-medium">🤠 Current backers</p>
+          <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
+            {GITHUB_USER_SPONSORS.map((sponsor) => (
+              <div key={sponsor.title} className="w-auto p-3 border rounded-2xl hover:shadow">
+                <a className="w-full h-full flex flex-row justify-start items-center gap-2" href={sponsor.url} target="_blank">
+                  <img className="w-6 sm:w-8 rounded-full" src={sponsor.logo} alt="" />
+                  <span className="text-lg sm:text-xl">{sponsor.title}</span>
+                </a>
+              </div>
+            ))}
+
+            <div className="w-auto p-3 border border-dashed rounded-2xl hover:shadow">
+              <a
+                className="w-full h-full flex flex-row justify-start items-center gap-2 opacity-40"
+                href="https://github.com/sponsors/usememos"
+                target="_blank"
+              >
+                <Icon.SmilePlus className="w-5 sm:w-7 h-auto" strokeWidth={1} />
+                <div className="h-auto flex flex-col justify-center items-start">
+                  <span className="text-lg">Become a sponsor</span>
+                </div>
+              </a>
+            </div>
+          </div>
+          <a className="mt-2 text-blue-600 hover:underline hover:opacity-80" href="https://github.com/sponsors/usememos" target="_blank">
+            <span className="">And more than 40+ sponsors in GitHub</span>
+            <Icon.ExternalLink className="ml-1 inline w-4 h-auto" />
+          </a>
         </div>
       </div>
     </>
