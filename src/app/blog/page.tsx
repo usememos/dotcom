2e59a84a186cc6c8aae18936e111c6b76718402b
Steven <stevenlgtm@gmail.com>
2024-06-10 19:15:18 +0800

    chore: tweak blog styles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,52 +17,50 @@ const Page = () => {
   const frontmatters = getBlogFrontmatters();
 
   return (
-    <>
-      <div className="w-full mx-auto py-2 sm:px-4 flex flex-col justify-start items-start">
-        <h1 className="w-full text-2xl text-start sm:text-4xl font-medium sm:font-bold mt-4">Blogs</h1>
-        <h2 className="text-lg mt-4">Get the latest articles from Memos</h2>
-        <div className="mt-6 w-full flex flex-col justify-start items-start gap-4 sm:gap-6">
-          {frontmatters.map((frontmatter) => {
-            const author = authorList.find((author) => author.name === frontmatter.author) as Author;
+    <div className="w-full mx-auto py-2 sm:px-4 flex flex-col justify-start items-start">
+      <h2 className="w-full text-start text-5xl sm:text-6xl font-medium sm:font-bold mt-4">Blogs</h2>
+      <h3 className="text-xl mt-4 leading-normal">Get the latest articles from Memos</h3>
+      <div className="mt-6 w-full grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6">
+        {frontmatters.map((frontmatter) => {
+          const author = authorList.find((author) => author.name === frontmatter.author) as Author;
 
-            return (
-              <React.Fragment key={frontmatter.slug}>
-                <div className="w-full bg-white p-4 border rounded-xl flex flex-row justify-start items-start gap-x-4">
-                  <div className="flex flex-1 flex-col sm:p-2 justify-start items-start">
-                    {frontmatter.feature_image && (
-                      <Link
-                        className="w-full sm:hidden mb-4 shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
-                        href={`/blog/${frontmatter.slug}`}
-                      >
-                        <img src={frontmatter.feature_image} alt="" />
-                      </Link>
-                    )}
-                    <Link className="text-lg !leading-tight sm:text-xl line-clamp-2 hover:text-blue-600" href={`/blog/${frontmatter.slug}`}>
-                      {frontmatter.title}
-                    </Link>
-                    {frontmatter.description && <p className="mt-2 text-gray-400 line-clamp-2">{frontmatter.description}</p>}
-                    <div className="mt-2 w-full flex flex-row justify-start items-center text-gray-500">
-                      <span>{author.name}</span>
-                      <Icon.Dot />
-                      <span>{dayjs(frontmatter.published_at).format("DD/MM/YYYY")}</span>
-                    </div>
-                  </div>
+          return (
+            <React.Fragment key={frontmatter.slug}>
+              <div className="w-full bg-white p-4 border rounded-xl flex flex-row justify-start items-start gap-x-4 hover:shadow">
+                <div className="flex flex-1 flex-col sm:p-2 justify-start items-start">
                   {frontmatter.feature_image && (
                     <Link
-                      className="hidden sm:block shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
+                      className="w-full sm:hidden mb-4 shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
                       href={`/blog/${frontmatter.slug}`}
                     >
-                      <img className="w-72" src={frontmatter.feature_image} alt="" />
+                      <img src={frontmatter.feature_image} alt="" />
                     </Link>
                   )}
+                  <Link className="text-lg !leading-tight sm:text-xl line-clamp-2 hover:text-blue-600" href={`/blog/${frontmatter.slug}`}>
+                    {frontmatter.title}
+                  </Link>
+                  {frontmatter.description && <p className="mt-2 text-gray-400 line-clamp-2">{frontmatter.description}</p>}
+                  <div className="mt-2 w-full flex flex-row justify-start items-center text-gray-500">
+                    <span>{author.name}</span>
+                    <Icon.Dot />
+                    <span>{dayjs(frontmatter.published_at).format("DD/MM/YYYY")}</span>
+                  </div>
                 </div>
-              </React.Fragment>
-            );
-          })}
-          <Subscription className="mt-4" />
-        </div>
+                {frontmatter.feature_image && (
+                  <Link
+                    className="hidden sm:block shrink-0 rounded-lg overflow-clip hover:opacity-80 hover:shadow"
+                    href={`/blog/${frontmatter.slug}`}
+                  >
+                    <img className="w-60" src={frontmatter.feature_image} alt="" />
+                  </Link>
+                )}
+              </div>
+            </React.Fragment>
+          );
+        })}
       </div>
-    </>
+      <Subscription className="mt-6" />
+    </div>
   );
 };
 
--- src/app/supporters/page.tsx
+++ src/app/supporters/page.tsx
@@ -19,73 +19,71 @@ const GITHUB_USER_SPONSORS = [
 
 const Page = () => {
   return (
-    <>
-      <div className="w-full flex flex-col justify-center items-start sm:px-12">
-        <h3 className="mt-12 mb-4 text-5xl sm:text-6xl leading-relaxed font-bold w-full">Thanks! 💗</h3>
-        <p className="text-xl max-w-3xl leading-normal">
-          {
-            "All donations directly support the development and operation of Memos. Recurring donations allow us to plan for the future. We deeply appreciate every donation — Thank you!"
-          }
-        </p>
-        <div className="w-full flex flex-col mt-12">
-          <p className="text-2xl sm:text-3xl font-medium">🦄 Sponsors</p>
-          <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
-            {SPONSORS.map((sponsor) => (
-              <div key={sponsor.title} className="w-auto p-4 border rounded-2xl hover:shadow">
-                <a className="w-full h-full flex flex-row justify-start items-center gap-2" href={sponsor.url} target="_blank">
-                  <img className="w-12 sm:w-16" src={sponsor.logo} alt="" />
-                  <span className="text-2xl sm:text-3xl">{sponsor.title}</span>
-                </a>
-              </div>
-            ))}
-
-            <div className="w-auto p-4 border border-dashed rounded-2xl hover:shadow">
-              <a
-                className="w-full h-full flex flex-row justify-start items-center gap-2 opacity-40"
-                href="https://github.com/sponsors/usememos"
-                target="_blank"
-              >
-                <Icon.HeartHandshake className="w-10 sm:w-14 h-auto" strokeWidth={1} />
-                <div className="h-auto flex flex-col justify-center items-start">
-                  <span className="text-2xl !leading-none">Your logo</span>
-                  <p className="opacity-70">Become a sponsor</p>
-                </div>
+    <div className="w-full flex flex-col justify-center items-start sm:px-12">
+      <h3 className="mt-12 mb-4 text-5xl sm:text-6xl leading-relaxed font-bold w-full">Thanks! 💗</h3>
+      <p className="text-xl max-w-3xl leading-normal">
+        {
+          "All donations directly support the development and operation of Memos. Recurring donations allow us to plan for the future. We deeply appreciate every donation — Thank you!"
+        }
+      </p>
+      <div className="w-full flex flex-col mt-12">
+        <p className="text-2xl sm:text-3xl font-medium">🦄 Sponsors</p>
+        <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
+          {SPONSORS.map((sponsor) => (
+            <div key={sponsor.title} className="w-auto p-4 border rounded-2xl hover:shadow">
+              <a className="w-full h-full flex flex-row justify-start items-center gap-2" href={sponsor.url} target="_blank">
+                <img className="w-12 sm:w-16" src={sponsor.logo} alt="" />
+                <span className="text-2xl sm:text-3xl">{sponsor.title}</span>
               </a>
             </div>
-          </div>
-        </div>
-        <div className="w-full flex flex-col mt-12">
-          <p className="text-2xl sm:text-3xl font-medium">🤠 Current backers</p>
-          <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
-            {GITHUB_USER_SPONSORS.map((sponsor) => (
-              <div key={sponsor.title} className="w-auto p-3 border rounded-2xl hover:shadow">
-                <a className="w-full h-full flex flex-row justify-start items-center gap-2" href={sponsor.url} target="_blank">
-                  <img className="w-6 sm:w-8 rounded-full" src={sponsor.logo} alt="" />
-                  <span className="text-lg sm:text-xl">{sponsor.title}</span>
-                </a>
-              </div>
-            ))}
+          ))}
 
-            <div className="w-auto p-3 border border-dashed rounded-2xl hover:shadow">
-              <a
-                className="w-full h-full flex flex-row justify-start items-center gap-2 opacity-40"
-                href="https://github.com/sponsors/usememos"
-                target="_blank"
-              >
-                <Icon.SmilePlus className="w-5 sm:w-7 h-auto" strokeWidth={1.5} />
-                <div className="h-auto flex flex-col justify-center items-start">
-                  <span className="text-lg">Become a backer</span>
-                </div>
-              </a>
-            </div>
+          <div className="w-auto p-4 border border-dashed rounded-2xl hover:shadow">
+            <a
+              className="w-full h-full flex flex-row justify-start items-center gap-2 opacity-40"
+              href="https://github.com/sponsors/usememos"
+              target="_blank"
+            >
+              <Icon.HeartHandshake className="w-10 sm:w-14 h-auto" strokeWidth={1} />
+              <div className="h-auto flex flex-col justify-center items-start">
+                <span className="text-2xl !leading-none">Your logo</span>
+                <p className="opacity-70">Become a sponsor</p>
+              </div>
+            </a>
           </div>
-          <a className="mt-2 text-blue-600 hover:underline hover:opacity-80" href="https://github.com/sponsors/usememos" target="_blank">
-            <span className="">And more than 40+ sponsors in GitHub</span>
-            <Icon.ExternalLink className="ml-1 inline w-4 h-auto" />
-          </a>
         </div>
       </div>
-    </>
+      <div className="w-full flex flex-col mt-12">
+        <p className="text-2xl sm:text-3xl font-medium">🤠 Current backers</p>
+        <div className="mt-4 sm:mt-6 flex flex-row flex-wrap gap-3 sm:gap-4">
+          {GITHUB_USER_SPONSORS.map((sponsor) => (
+            <div key={sponsor.title} className="w-auto p-3 border rounded-2xl hover:shadow">
+              <a className="w-full h-full flex flex-row justify-start items-center gap-2" href={sponsor.url} target="_blank">
+                <img className="w-6 sm:w-8 rounded-full" src={sponsor.logo} alt="" />
+                <span className="text-lg sm:text-xl">{sponsor.title}</span>
+              </a>
+            </div>
+          ))}
+
+          <div className="w-auto p-3 border border-dashed rounded-2xl hover:shadow">
+            <a
+              className="w-full h-full flex flex-row justify-start items-center gap-2 opacity-40"
+              href="https://github.com/sponsors/usememos"
+              target="_blank"
+            >
+              <Icon.SmilePlus className="w-5 sm:w-7 h-auto" strokeWidth={1.5} />
+              <div className="h-auto flex flex-col justify-center items-start">
+                <span className="text-lg">Become a backer</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <a className="mt-2 text-blue-600 hover:underline hover:opacity-80" href="https://github.com/sponsors/usememos" target="_blank">
+          <span className="">And more than 40+ sponsors in GitHub</span>
+          <Icon.ExternalLink className="ml-1 inline w-4 h-auto" />
+        </a>
+      </div>
+    </div>
   );
 };
 
