a7521958418d1cefb0211c4c85da2e1f4bfab29b
Steven <stevenlgtm@gmail.com>
2024-08-07 23:43:13 +0800

    chore: fix branding padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
       <div className="mt-4 sm:mt-6 w-full flex flex-col justify-start items-center">
         <LatestVersion />
       </div>
-      <div className="w-full flex flex-col justify-center items-center sm:px-16">
+      <div className="w-full flex flex-col justify-center items-center px-4 sm:px-16">
         <h2 className="w-full max-w-3xl text-center text-4xl sm:text-5xl font-medium sm:font-bold mt-4 mb-4">
           A privacy-first, lightweight note-taking service
         </h2>
