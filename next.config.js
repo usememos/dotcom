de35de1ea29e54f40caf9576020be071aced42f2
johnnyjoy <yourselfhosted@gmail.com>
2024-11-13 20:17:10 +0800

    chore(dev): add code insepector plugin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const { codeInspectorPlugin } = require("code-inspector-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(codeInspectorPlugin({ bundler: "webpack" }));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
