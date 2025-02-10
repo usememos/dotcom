100d924b9815f8fa0db025639c48ad7509291da4
Steven <stevenlgtm@gmail.com>
2025-02-10 22:07:45 +0800

    chore: update latest version

--- src/components/LatestVersion.tsx
+++ src/components/LatestVersion.tsx
@@ -1,30 +1,22 @@
 "use client";
 
-import axios from "axios";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import Icon from "./Icon";
 
 const LatestVersion = () => {
-  const [version, setVersion] = useState<string>("");
-
-  useEffect(() => {
-    axios.get<string>("/api/version").then(({ data }) => {
-      setVersion(data);
-    });
-  }, []);
+  const latestVerion = "v0.24.0";
 
   return (
     <Link
       className={`${
-        !version && "invisible"
+        !latestVerion && "invisible"
       } flex flex-row justify-center items-center my-2 mb-4 px-4 py-2 rounded-full text-sm bg-orange-100 text-gray-800 border border-orange-200 hover:shadow-sm hover:bg-orange-200`}
       href="https://github.com/usememos/memos/releases"
       target="_blank"
     >
       <span className="mr-2">🎉</span>
       Released
-      <span className="font-medium ml-1">v{version}</span>
+      <span className="font-medium ml-1">{latestVerion}</span>
       <Icon.ArrowRight className="w-4 h-auto ml-1" />
     </Link>
   );
