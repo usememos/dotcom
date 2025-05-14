90e124dad3736e41d9bda5014a91f95e4a7e545c
Johnny <yourselfhosted@gmail.com>
2025-05-14 23:50:06 +0800

    chore: update latest version

--- src/components/LatestVersion.tsx
+++ src/components/LatestVersion.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Icon from "./Icon";
 
 const LatestVersion = () => {
-  const latestVerion = "v0.24.2";
+  const latestVerion = "v0.24.3";
 
   return (
     <Link
